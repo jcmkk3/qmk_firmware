4a9fe86dabafca2833b959eafb4d4338df5f8827
Kyle Mitchell <45919695+jcmkk3@users.noreply.github.com>
2021-12-26 21:46:24 -0800

    Update config.h

--- keyboards/broom/thephantom/config.h
+++ keyboards/broom/thephantom/config.h
@@ -48,9 +48,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define UNUSED_PINS
 
-/* COL2ROW, ROW2COL*/
-#define DIODE_DIRECTION NO_DIODE
-
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
